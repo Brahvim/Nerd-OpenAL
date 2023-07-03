59ddc89baa2f7ae85013c215beff3bd11a19a2f0
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-07-03 12:18:13 +0530

    "Reflecting upon Nerd's API changes".
    *- Common sentence in these commit logs.*

--- src/com/brahvim/nerd/openal/NerdAlExt.java
+++ src/com/brahvim/nerd/openal/NerdAlExt.java
@@ -38,10 +38,9 @@ public class NerdAlExt extends NerdExt {
 
 		// When the scene is changed, delete unnecessary OpenAL data:
 		p_builder.addSketchConstructionListener(s -> s.getSceneManager()
-				.addSceneChangeListener((a, p, c) -> {
+				.addSceneChangeListener((a, p, c) -> { // The mispronounced alphabets joke.
 					if (p != null)
 						toRet.scenelyDisposal();
-					toRet.unitSize = NerdAl.UNIT_SIZE_3D_PARK_SCENE;
 				}));
 
 		// We have framely callbacks, yes:
@@ -54,49 +53,34 @@ public class NerdAlExt extends NerdExt {
 	}
 
 	protected Consumer<NerdSketch> provideDrawListener(final NerdSketch p_sketch, final NerdAl p_alInst) {
-		switch (p_sketch.RENDERER_NAME) {
-			case PConstants.P2D, PConstants.P3D -> {
-				// I wanted to declare this lambda as an anon class instead, but I wanted to
-				// watch this trick where I have a variable from outside the lambda work there.
-				// ...It does!:
-				final PVector lastCameraPos = new PVector();
-
-				return s -> {
-					// Process everything, every frame!:
-					p_alInst.framelyCallback();
-
-					final NerdAbstractCamera camera = p_sketch.getCamera();
-					final PVector camPos = camera.getPos(), camUp = camera.getUp();
-
-					p_alInst.setListenerOrientation(camUp.x, camUp.y, camUp.z);
-
-					p_alInst.setListenerVelocity(
-							camPos.x - lastCameraPos.x,
-							camPos.y - lastCameraPos.y,
-							camPos.z - lastCameraPos.z);
-					// PVector.div((PVector.sub(camPos, lastCameraPos)).array(),
-					// this.unitSize));
-
-					// JIT-style optimization + protection from `0`!:
-					if (p_alInst.unitSize == 1.0f || p_alInst.unitSize == 0.0f) {
-						p_alInst.setListenerPosition(camPos.x, camPos.y, camPos.z);
-					} else {
-						// final PVector listenerPos = PVector.div(camPos, toRet.unitSize);
-						// toRet.setListenerPosition(listenerPos.x, listenerPos.y, listenerPos.z);
-						p_alInst.setListenerPosition(
-								camPos.x / p_alInst.unitSize,
-								camPos.y / p_alInst.unitSize,
-								camPos.z / p_alInst.unitSize);
-					}
-
-					lastCameraPos.set(camPos);
-				};
-			}
-		}
-
 		// Renderer does not have a camera? No worries! Users handle this stuff now!
 		// We just... Process everything, every frame! (As we always do! ^-^):
-		return s -> p_alInst.framelyCallback();
+		if (PConstants.P3D.equals(p_sketch.SKETCH_SETTINGS.RENDERER_NAME))
+			return s -> p_alInst.framelyCallback();
+
+		// I wanted to declare this lambda as an anon class instead, but I wanted to
+		// watch this trick where I have a variable from outside the lambda work there.
+		// ...It does!:
+		final PVector lastCameraPos = new PVector();
+
+		return s -> {
+			// Process everything, every frame!:
+			p_alInst.framelyCallback();
+
+			final NerdAbstractCamera camera = p_sketch.getNerdGraphics().currentCamera;
+			final PVector camPos = camera.getPos(), camUp = camera.getUp();
+
+			p_alInst.setListenerOrientation(camUp.x, camUp.y, camUp.z);
+
+			p_alInst.setListenerVelocity(
+					camPos.x - lastCameraPos.x,
+					camPos.y - lastCameraPos.y,
+					camPos.z - lastCameraPos.z);
+
+			p_alInst.setListenerPosition(camPos.x, camPos.y, camPos.z);
+
+			lastCameraPos.set(camPos);
+		};
 	}
 
 }
